90ae131f604de536e7404b02d39497774b66b8e7
amir <amir6665aqq@gmail.com>
2022-03-11 14:02:06 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int nomre;
+double sum=0;
+int avg=0;
+int Counter=-1;
+do
+{
+    System.Console.WriteLine("nomre bede");
+    nomre=Convert.ToInt32(Console.ReadLine());
+    sum=sum+nomre;
+    Counter++;
+    avg=avg=nomre;
+} while (nomre<=20 && nomre !=0);
+System.Console.WriteLine(sum/Counter);
